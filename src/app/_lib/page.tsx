b33bc2966b5b0e8e9d735ec1b5e5b2cfc647bd4a
faisal-mohamed <mohamedfaisalm@hotmail.com>
2025-01-06 20:46:55 +0530

    private folders

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,6 @@
+export default function Lib() {
+
+    return <div>
+        <h1>This is a Private folder and Cannot be accessible by routing due to the addition of _ infront of the fileName</h1>
+    </div>
+}
