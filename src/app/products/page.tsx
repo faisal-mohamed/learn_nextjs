460dbf4a1ad25ad8774d9d18709834c9521fb598
faisal-mohamed <mohamedfaisalm@hotmail.com>
2025-01-08 11:55:02 +0530

    Navigation using Link Component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
 export default function MainProductsPage() {
 
 
     return <div>
         Main Products Page
+
+        <Link href="/products/1">Product 1</Link>
+        <Link href="/products/2">Product 2</Link>
+        <Link href="/products/3">Product 3</Link>
     </div>
 }
