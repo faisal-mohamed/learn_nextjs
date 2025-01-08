fcebc14ff2434d8e4802064127cc221a286bf26f
faisal-mohamed <mohamedfaisalm@hotmail.com>
2025-01-08 11:16:03 +0530

    layout

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,11 @@
+export default function ReviewLayout({children}: {
+    children: React.ReactNode
+}) {
+
+
+
+    return <div>
+        Featured Products in Product Id Page
+        {children}
+    </div>
+}
