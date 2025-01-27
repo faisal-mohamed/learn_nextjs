15ded354ea06d3b8911665d2f06188e22be61e8d
faisal-mohamed <mohamedfaisalm@hotmail.com>
2025-01-27 20:02:11 +0530

    GET & POST route handlers

---
+++ src/app/route-handlers/data.ts
@@ -0,0 +1,19 @@
+export const users = [{
+    name: 'John',
+    age: 30
+
+}, {
+    name: 'Jane',
+    age: 25
+}, {
+    name: 'Jack',
+    age: 20
+}, {
+    name: 'Jill',
+    age: 15
+
+}, {
+    name: 'Joe',
+    age: 10
+}];
+
--- src/app/route-handlers/route.ts
+++ src/app/route-handlers/route.ts
@@ -1,3 +1,22 @@
+import { users } from "./data"
+
+
+
 export  async function GET() {
-    return new Response('First Get Request')
+    return  Response.json(users);
+}
+
+
+export async function POST(req: Request) {
+    const res = await req.json();
+    console.log(res)
+    
+    const newUser = {
+        name: res.name,
+        age: res.age
+    }
+
+    users.push(newUser);
+
+    return  Response.json(newUser)
 }
